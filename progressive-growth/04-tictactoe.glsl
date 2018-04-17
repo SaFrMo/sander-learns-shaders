0b300224049f1641ea88aeffaefb06a3b7f1ca7c
Sander Moolin <sandermoolin@gmail.com>
2018-04-17 14:16:16 -0400

    Added line down right side

--- progressive-growth/04-tictactoe.glsl
+++ progressive-growth/04-tictactoe.glsl
@@ -4,6 +4,14 @@ precision mediump float;
 
 #define PI 3.14159265359
 
+// Let's make a tic-tac-toe board!
+
+// Our end result should look like this:
+// X | X | O
+// O | X | O
+// X | O | O
+// (with a line through the right-side O's)
+
 float circle(in vec2 st){
 	float o = 0.;
 	float d = distance(st, vec2(0.5));
@@ -36,8 +44,6 @@ float cross(in vec2 st){
 }
 
 void main() {
-	// Let's make a tic-tac-toe board!
-
 	// bottom left to top right = (0., 0.) to (1., 1.)
 	vec2 st = gl_FragCoord.xy/u_resolution;
 
@@ -47,49 +53,53 @@ void main() {
 	// start with a black background
 	float color = 0.;
 
-	// let's make our board look like this:
-	// X | X | O
-	// O | X | O
-	// X | O | O
-	// (with a line through the right-side O's)
-
 	// get our relative coordinates
 	// bottom left to top right: (0., 0.) to (2., 2.)
 	vec2 rel = vec2(floor(st.x), floor(st.y));
 
 	// normalize to (0., 0.) to (1., 1.) in our current square
-	st = fract(st);
+	vec2 uv = fract(st);
 
 	float borderStroke = 0.02;
 
 	// all x = 0 and x = 1 tiles have a border on the right
 	if( rel.x == 0. || rel.x == 1. ){
-		color = step(1. - borderStroke / 2., st.x);
+		color = step(1. - borderStroke / 2., uv.x);
 	}
 	// all x = 1 and x = 2 tiles have a border on the left
 	if( rel.x == 1. || rel.x == 2. ){
-		color += step(st.x, borderStroke / 2.);
+		color += step(uv.x, borderStroke / 2.);
 	}
 	// y = 0 and y = 1 have a border on the top
 	if( rel.y == 0. || rel.y == 1. ){
-		color += step(1. - borderStroke / 2., st.y);
+		color += step(1. - borderStroke / 2., uv.y);
 	}
 	// all y = 1 and y = 2 tiles have a border on the bottom
 	if( rel.y == 1. || rel.y == 2. ){
-		color += step(st.y, borderStroke / 2.);
+		color += step(uv.y, borderStroke / 2.);
 	}
 
-	// all x = 2 squares get a circle
+	// all x = 2 squares get a circle...
 	if( rel.x == 2. ){
-		color += circle(st);
+		color += circle(uv);
 	} else if( rel.x == 0. && rel.y == 1. ){
-		color += circle(st);
+		// ... and so does (0., 1.)...
+		color += circle(uv);
 	} else if( rel.x == 1. && rel.y == 0. ){
-		color += circle(st);
+		// ... and so does (1., 0.)
+		color += circle(uv);
 	} else {
-		color += cross(st);
+		// Everything else gets a cross
+		color += cross(uv);
 	}
 
+	// Now let's draw a line down the middle right
+	float stroke = 0.15;
+	float lineCenter = 2.5;
+	float yPadding = 0.05;
+	vec2 ur = step(vec2(lineCenter - stroke / 2., yPadding), st);
+	vec2 dl = 1. - step(vec2(lineCenter + stroke / 2., 3. - yPadding), st);
+	color += ur.x * ur.y * dl.x * dl.y;
 
     vec3 c = vec3(color);
 
